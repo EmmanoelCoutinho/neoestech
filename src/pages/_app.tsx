bdc9a44e289e1720fea24b3f65db990e8e7e1a85
EmmanoelCoutinho <emmanoelcesar967@gmail.com>
2022-10-09 22:12:31 -0300

    criação do hook de providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,16 @@
 import type { AppProps } from "next/app";
 
-import { UserProvider } from "../contexts/userContext";
-
-import { ConfigProvider } from "antd";
 import ptBR from "antd/lib/locale/pt_BR";
 import "antd/dist/antd.css";
+
+import { AllProviders } from "../utils/AllProviders";
 
 import "../styles/main.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ConfigProvider locale={ptBR}>
-      <UserProvider>
-        <Component {...pageProps} />
-      </UserProvider>
-    </ConfigProvider>
+    <AllProviders locale={ptBR}>
+      <Component {...pageProps} />
+    </AllProviders>
   );
 }
---
+++ src/utils/AllProviders.tsx
@@ -0,0 +1,17 @@
+import { ConfigProvider } from "antd";
+import { UserProvider } from "../contexts/userContext";
+import { NewPriceModalProvider } from "../contexts/newPriceModal";
+
+const composeProviders =
+  (...providers: any[]) =>
+  (props: any) =>
+    providers.reduceRight(
+      (children, Provider) => <Provider {...props}>{children}</Provider>,
+      props.children
+    );
+
+export const AllProviders = composeProviders(
+  ConfigProvider,
+  UserProvider,
+  NewPriceModalProvider
+);
